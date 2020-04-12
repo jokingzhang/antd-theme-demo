2822501e109913b05e1c6618a08cacd839c2629d
zhangbx <zhangbx@didiglobal.com>
2020-04-12 17:46:20 +0800

    Updates

---
+++ precache-manifest.accb855582e927834a0df14f7afa20e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70be588d053d2409f56403ec078f0d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0a875461ca68f3765cd",
+    "url": "/static/css/2.ca0e0631.chunk.css"
+  },
+  {
+    "revision": "a390e9c046708d143e6f",
+    "url": "/static/css/main.bb6b2272.chunk.css"
+  },
+  {
+    "revision": "e0a875461ca68f3765cd",
+    "url": "/static/js/2.e87e2a76.chunk.js"
+  },
+  {
+    "revision": "10b10bffff63f89170a0f107f71375bb",
+    "url": "/static/js/2.e87e2a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a390e9c046708d143e6f",
+    "url": "/static/js/main.4882c78a.chunk.js"
+  },
+  {
+    "revision": "4dd05a018ca0daa3a43a",
+    "url": "/static/js/runtime-main.8243941a.js"
+  },
+  {
+    "revision": "471ebf2951f99818f859562bf339801d",
+    "url": "/static/media/chahua.471ebf29.png"
+  },
+  {
+    "revision": "cc562d8c2c7e5a654ff88327e2bf77b8",
+    "url": "/static/media/chong.cc562d8c.png"
+  },
+  {
+    "revision": "9baa1fb0e87fdfd495b046c019ffb61c",
+    "url": "/static/media/lizard.9baa1fb0.png"
+  }
+]);
